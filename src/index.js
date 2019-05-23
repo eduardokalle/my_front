85d42a8d6a91ccb7c0894d575d772afff5c8d22c
Eduardo Calle <49166547+eduardokalle@users.noreply.github.com>
2019-05-23 07:33:43 +0200

    Revert "pv_4"
    This reverts commit ee02fe68098a40c89128db006c46bb9b63348b55.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
-const express = require ('express');
-const app = express ();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Route, BrowserRouter, Switch } from 'react-router-dom';
+
+import {Crud , Login}  from './pages';
+import * as serviceWorker from './serviceWorker';
+import Layout from './Component/Layout';
+
+import './index.css';
+import 'antd/dist/antd.css';
+//import App from './App';
 
 
-//Settings
+function App() {
+    return (
+        <BrowserRouter>
+          <Layout>
+            <Switch>
+             <Route exact path="/" component={Crud} />
+             <Route  path="/login" component={Login} />
+            </Switch>
+          </Layout>
+        </BrowserRouter>
+    );
+  }
 
-//Middlewares
 
-//Routes
+ReactDOM.render(<App />, document.getElementById('root'));
 
-//Static files
-
-//Starting on server
-
-app.listen (3000, () =>{
-    console.log('server on port 3000');
-});
-
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
