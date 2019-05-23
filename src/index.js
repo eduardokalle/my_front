ee02fe68098a40c89128db006c46bb9b63348b55
Eduardo Calle <49166547+eduardokalle@users.noreply.github.com>
2019-05-23 07:30:13 +0200

    pv_4

--- src/index.js
+++ src/index.js
@@ -1,33 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Route, BrowserRouter, Switch } from 'react-router-dom';
-
-import {Crud , Login}  from './pages';
-import * as serviceWorker from './serviceWorker';
-import Layout from './Component/Layout';
-
-import './index.css';
-import 'antd/dist/antd.css';
-//import App from './App';
+const express = require ('express');
+const app = express ();
 
 
-function App() {
-    return (
-        <BrowserRouter>
-          <Layout>
-            <Switch>
-             <Route exact path="/" component={Crud} />
-             <Route  path="/login" component={Login} />
-            </Switch>
-          </Layout>
-        </BrowserRouter>
-    );
-  }
+//Settings
+
+//Middlewares
+
+//Routes
+
+//Static files
+
+//Starting on server
+
+app.listen (3000, () =>{
+    console.log('server on port 3000');
+});
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
